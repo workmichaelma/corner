8fc5e0b72c13742daae6acd3f76a7c28e115989f
Michael Ma <work.michael.ma@gmail.com>
2021-04-05 01:58:05 +0800

    Update page name and favicon

--- app/pages/match/_id.vue
+++ app/pages/match/_id.vue
@@ -1,36 +1,45 @@
 <template>
-  <v-layout
-    justify-center
-    align-center
-  >
+  <v-layout justify-center align-center>
     <v-flex xs12 sm8 md10>
-      <full-match v-if="!isUndefined(match) && !isEmpty(match)" v-bind="{match}" />
+      <full-match
+        v-if="!isUndefined(match) && !isEmpty(match)"
+        v-bind="{ match }"
+      />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import { isEmpty, isUndefined } from 'lodash'
-import FullMatch from '~/components/fullMatch'
+import { isEmpty, isUndefined, get } from "lodash";
+import FullMatch from "~/components/fullMatch";
 
 export default {
-  async asyncData ({ params, store }) {
-    const { id } = params
+  async asyncData({ params, store }) {
+    const { id } = params;
     if (id) {
-      const exist = await store.dispatch('match/fetchMatch', {
+      const exist = await store.dispatch("match/fetchMatch", {
         id
-      })
-      console.log({ exist })
+      });
+      console.log({ exist });
     }
+  },
+  head() {
+    return {
+      title: `${get(this.match, "homeTeam.teamName", "")} 對 ${get(
+        this.match,
+        "awayTeam.teamName",
+        ""
+      )} - ${get(this.match, "league.name", "")}`
+    };
   },
   components: {
     FullMatch
   },
   computed: {
-    match () {
-      const { id } = this.$route.params || {}
+    match() {
+      const { id } = this.$route.params || {};
       if (id) {
-        return this.$store.state.match[id]
+        return this.$store.state.match[id];
       }
     }
   },
@@ -38,8 +47,7 @@ export default {
     isEmpty,
     isUndefined
   }
-}
+};
 </script>
 
-<style lang="stylus">
-</style>
+<style lang="stylus"></style>
