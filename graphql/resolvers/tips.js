6fc50d7de8be92c972715d4ec4d91d48e510a025
Michael Ma <work.michael.ma@gmail.com>
2021-11-14 18:37:53 +0800

    Return chinese on tips

--- graphql/resolvers/tips.js
+++ graphql/resolvers/tips.js
@@ -1,6 +1,27 @@
 const moment = require("moment");
 const TipsSchema = require("../mongo/schema/Tips");
 
+const transformBet = ({ betItem, betType }) => {
+  const type = betType.toString();
+  const item = betItem.toString();
+  if (type.toString() === "CHL") {
+    return {
+      betType: "角球",
+      betItem: item === "H" ? "大" : "細",
+    };
+  } else if (type === "HAD") {
+    return {
+      betType: "主客和",
+      betItem: item === "H" ? "主" : item === "D" ? "和" : "客",
+    };
+  } else if (type === "HDC") {
+    return {
+      betType: "讓球",
+      betItem: item === "H" ? "主" : "客",
+    };
+  }
+};
+
 module.exports = {
   Query: {
     tips: async (obj, args, context, info) => {
@@ -12,11 +33,17 @@ module.exports = {
       return tips.map(({ match, ...t }) => {
         const { datetime } = match[0];
         const isStarted = moment(datetime).isBefore(moment());
+        const { betItem, betType } = transformBet({
+          betItem: t.betItem,
+          betType: t.betType,
+        });
 
         return {
           match: match[0],
           isStarted,
           ...t,
+          betItem,
+          betType,
         };
       });
       // const { docs } = await MatchSchema.getSchedule({
