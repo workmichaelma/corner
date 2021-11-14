01b7274b2db81779c633b4b7ad7dd04a215dba60
Michael Ma <work.michael.ma@gmail.com>
2021-11-14 18:03:30 +0800

    Init graphql tips

---
+++ graphql/mongo/schema/Tips.js
@@ -0,0 +1,53 @@
+const { isUndefined } = require("lodash");
+const mongoose = require("mongoose");
+const MatchSchema = require("./Match");
+const TeamSchema = require("./Team");
+const LeagueSchema = require("./League");
+const Schema = mongoose.Schema;
+
+const TipsSchema = new Schema({
+  date: String,
+  matchId: {
+    type: Schema.Types.ObjectId,
+  },
+  betType: String,
+  betItem: String,
+  betOdd: String,
+  betGrade: String,
+});
+
+TipsSchema.virtual("match", {
+  ref: "Match",
+  localField: "matchId",
+  foreignField: "id",
+});
+
+TipsSchema.statics.getTips = async ({ date }) => {
+  const tips = await Tips.find({
+    date,
+  })
+    .populate({
+      path: "match",
+      model: MatchSchema,
+      populate: [
+        {
+          path: "home",
+          model: TeamSchema,
+        },
+        {
+          path: "away",
+          model: TeamSchema,
+        },
+        {
+          path: "league",
+          model: LeagueSchema,
+        },
+      ],
+    })
+    .lean()
+    .sort({ betGrade: 1 });
+
+  return tips;
+};
+
+module.exports = Tips = mongoose.model("tips", TipsSchema, "tips");
--- graphql/resolvers/tips.js
+++ graphql/resolvers/tips.js
@@ -1,25 +1,37 @@
-const map = require("lodash/map");
-const compact = require("lodash/compact");
-const MatchSchema = require("../mongo/schema/Match");
-
-const fetchTips = require("../utils/tips");
+const moment = require("moment");
+const TipsSchema = require("../mongo/schema/Tips");
 
 module.exports = {
   Query: {
     tips: async (obj, args, context, info) => {
-      const { docs } = await MatchSchema.getSchedule({
-        limit: 30,
+      const today = moment().format("YYYY-MM-DD");
+      const tips = await TipsSchema.getTips({
+        date: today,
       });
 
-      const matches = await Promise.all(
-        map(docs, async (m) => {
-          return fetchTips(m);
-        })
-      );
+      return tips.map(({ match, ...t }) => {
+        const { datetime } = match[0];
+        const isStarted = moment(datetime).isBefore(moment());
 
-      return {
-        matches: compact(matches),
-      };
+        return {
+          match: match[0],
+          isStarted,
+          ...t,
+        };
+      });
+      // const { docs } = await MatchSchema.getSchedule({
+      //   limit: 30,
+      // });
+
+      // const matches = await Promise.all(
+      //   map(docs, async (m) => {
+      //     return fetchTips(m);
+      //   })
+      // );
+
+      // return {
+      //   matches: compact(matches),
+      // };
     },
   },
 };
